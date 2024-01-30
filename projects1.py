fbb605a749522c171650bdd930cf877dc417a2ba
jacob0635 <108902172+jacob0635@users.noreply.github.com>
2024-01-30 21:30:32 +0100

    first commit

---
+++ projects1.py
@@ -0,0 +1,18 @@
+
+# Machine Learning makes decision or predictions using data.
+# We going to use Linear Regressing to predict a module
+
+# Macine Learning worksa in 2 ways, (1) learning (2) Prediction
+
+import numpy as np
+a1 = np.array([1,2,3,4])
+print(a1)
+
+print(a1.dtype)
+print(a1.shape)
+
+a2 = ([5,6,7,8])
+print(a2)
+print(a2*2)
+print(a1*a2)
+print(a1 + a2)
